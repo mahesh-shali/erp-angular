722e3e96f858122271793ac04b06962ee373ca1f
maheshshali25 <maiya9986@gmail.com>
2025-06-08 21:16:06 +0530

    updated

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Navbar } from './components/navbar/navbar';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Home, Navbar, Footer],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected title = 'client';
