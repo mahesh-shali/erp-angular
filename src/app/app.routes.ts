f760bc9b4407f1b5a4d305ac2f590bdf58d68a32
maheshshali25 <maiya9986@gmail.com>
2025-06-12 21:10:08 +0530

    updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,14 @@ import { Routes } from '@angular/router';
 // Make sure the correct path and export are used for LoginComponent
 import { Login } from './pages/login/login';
 import { Home } from './pages/home/home';
+import { Dashboard } from './pages/superAdmin/dashboard/dashboard';
 
 export const routes: Routes = [
   { path: 'login', component: Login },
   { path: 'home', component: Home },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: '**', redirectTo: '/login' },
+
+  //super admin routes
+  { path: 'dashboard', component: Dashboard },
 ];
