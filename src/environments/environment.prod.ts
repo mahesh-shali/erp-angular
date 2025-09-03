b80227ab1844f7d48f25dfc5623f0d3cd07ffc75
maheshshali25 <maiya9986@gmail.com>
2025-09-03 20:49:01 +0530

    updated

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://erp-server-8yc3.onrender.com',
+  aiUrl: 'https://ai-wmzj.onrender.com',
+};
---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://staging-erp-server.onrender.com',
+  aiUrl: 'https://staging-ai-server.onrender.com',
+};
