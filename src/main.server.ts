7cdfb5041b069b16d7924a18f235e82646698d20
maheshshali25 <maiya9986@gmail.com>
2025-09-06 19:14:42 +0530

    updated package.json

--- src/main.server.ts
+++ src/main.server.ts
@@ -2,7 +2,8 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { App } from './app/app';
 import { config } from './app/app.config.server';
 import { enableProdMode } from '@angular/core';
-import { environment } from './environments/environment';
+// import { environment } from './environments/environment';
+import { environment } from './environments/environment.prod';
 
 if (environment.production) {
   enableProdMode();
