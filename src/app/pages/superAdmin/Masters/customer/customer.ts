1473294a1e12775a8b1b115ecf55f6651dc5550a
maheshshali25 <maiya9986@gmail.com>
2025-10-01 12:42:35 +0530

    up

--- src/app/pages/superAdmin/Masters/customer/customer.ts
+++ src/app/pages/superAdmin/Masters/customer/customer.ts
@@ -10,44 +10,12 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './customer.scss',
 })
 export class Customer implements OnInit {
+  isLoadingCustomer = true;
   roleId: number = 1;
-  isLoading = true;
-  searchText = '';
-  selectedUserId: number | null = null;
 
-  userFormData: any = {
-    name: '',
-    organization: '',
-    roleId: '',
-    email: '',
-    password: '',
-  };
-
-  staticOrgs = ['Google', 'Microsoft', 'Amazon', 'Tesla'];
-  staticRoles = [
-    { id: 1, name: 'Super Admin' },
-    { id: 2, name: 'Admin' },
-    { id: 3, name: 'Manager' },
-    { id: 4, name: 'User' },
-  ];
-  staticUsers = [
-    { id: 1, name: 'Alice', role: 'Admin' },
-    { id: 2, name: 'Bob', role: 'Manager' },
-    { id: 3, name: 'Charlie', role: 'User' },
-  ];
   ngOnInit(): void {
     setTimeout(() => {
-      this.isLoading = false;
+      this.isLoadingCustomer = false;
     }, 2000);
-  }
-  resetUserForm() {
-    this.userFormData = {
-      name: '',
-      organization: '',
-      roleId: '',
-      email: '',
-      password: '',
-    };
-    this.selectedUserId = null;
   }
 }
